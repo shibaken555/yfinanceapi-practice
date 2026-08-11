31b5a93c83e374ad5207442b105686e367188b13
shibaken <ychaishan@gmail.com>
2026-08-11 10:53:22 +0900

    関数名がタイポしていたので修正

--- ts/practice3.js
+++ ts/practice3.js
@@ -3,11 +3,11 @@ const invesmentReturn = 1.3;
 const percentageOfIr = (invesmentReturn - 1) * 100;
 console.log("投資の元本は" + invesmentPrincipal + "円です。");
 console.log("投資のリターンは" + percentageOfIr + "%です。");
-function calculatetotalAmount(productPrice, taxRate) {
+function calculateTotalAmount(productPrice, taxRate) {
     let totalAmount = productPrice * taxRate;
     return totalAmount;
 }
 console.log("商品価格は" + "1000円です");
 console.log("消費税率は" + "10%です");
-console.log(calculatetotalAmount(1000, 1.1));
+console.log(calculateTotalAmount(1000, 1.1));
 export {};
--- ts/practice3.ts
+++ ts/practice3.ts
@@ -6,11 +6,11 @@ const percentageOfIr:number = (invesmentReturn-1)*100;
 console.log("投資の元本は"+invesmentPrincipal+"円です。");
 console.log("投資のリターンは"+percentageOfIr+"%です。");
 
-function calculatetotalAmount(productPrice:number,taxRate:number):number{
+function calculateTotalAmount(productPrice:number,taxRate:number):number{
    let totalAmount:number = productPrice * taxRate;
    return totalAmount;
 }
 
 console.log("商品価格は" + "1000円です");
 console.log("消費税率は" + "10%です");
-console.log(calculatetotalAmount(1000,1.1));
+console.log(calculateTotalAmount(1000,1.1));

1064b6c1ad285100025a99141a98e336661cb6bd
shibaken <ychaishan@gmail.com>
2026-08-11 17:43:48 +0900

    Javaで書いたfizzbuzz

---
+++ java/practice2.java
@@ -0,0 +1,15 @@
+public class practice2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 15 == 0) {
+                System.out.println("fizzbuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}

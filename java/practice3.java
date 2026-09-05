f4c6570238e48b9331726ca191aab87a3ae2c2ce
shibaken <ychaishan@gmail.com>
2026-09-06 01:03:19 +0900

    Mapの練習をしたコードを追加

---
+++ java/practice3.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class practice3 {
+
+    // 重複している値を検出し全て削除するコード
+    public static void main(String[] args) {
+        // 対象のMap。appleが重複しているので、削除対象の値となる
+        Map<Integer, String> fruitMap = new HashMap<>();
+        fruitMap.put(1, "apple");
+        fruitMap.put(2, "banana");
+        fruitMap.put(3, "apple");
+        fruitMap.put(4, "kiwi");
+        fruitMap.put(5, "apple");
+        System.out.println("fruitMap" + fruitMap);
+
+        // Map内の値を数えるためのリストを作成
+        List<String> valueList = new ArrayList<>();
+        for (String values : fruitMap.values()) {
+            valueList.add(values);
+        }
+        System.out.println("valueList" + valueList);
+
+        // 値の出現回数を数えるMapを作成
+        Map<String, Integer> countsMap = new HashMap<>();
+        for (String word : valueList) {
+            countsMap.put(word, countsMap.getOrDefault(word, 0) + 1);
+        }
+        System.out.println("countsMap" + countsMap);
+
+        // 出現回数が2回以上の値を削除する
+        countsMap.entrySet().removeIf(entry -> entry.getValue() > 2);
+        System.out.println(countsMap);
+
+    }
+}
